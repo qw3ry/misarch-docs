e46ba2e42a6197cbfc49ab46512ecc8c9ca56fc6
Tobias Eisenreich <tobias.eisenreich@tum.de>
2026-06-02 17:02:38 +0200

    Make the home page redirection work as expected

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     const history = useHistory();
 
     useEffect(() => {
-        history.push("/docs");
+        history.push("/misarch-docs/docs");
     }, []);
 
     return null;
