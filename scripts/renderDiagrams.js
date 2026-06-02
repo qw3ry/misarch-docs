d5b31f70ba1b13cd68da0919bf5fae7788079f4f
Tobias Eisenreich <tobias.eisenreich@tum.de>
2026-06-02 16:38:43 +0200

    Render journey diagrams with `render-diagrams`

--- scripts/renderDiagrams.js
+++ scripts/renderDiagrams.js
@@ -1,32 +1,58 @@
 const fs = require("fs");
 const path = require("path");
-const { execSync } = require("child_process");
+const { exec } = require("child_process");
 
-const inputFolder = "diagrams";
-const outputFolder = "static/renderedDiagrams";
+function renderDiagram(inputFolder, file, outputFolder, cliBuilder, variant = "") {
+    const baseName = file.substring(0, file.lastIndexOf("."));
+    const inputFilePath = path.join(inputFolder, file);
+    const outputFileName = baseName + (variant ? "-" + variant : "") + ".svg";
+    const outputFilePath = path.join(outputFolder, outputFileName);
 
-fs.readdir(inputFolder, (err, files) => {
-    if (err) {
-        console.error("Error reading directory:", err);
-        return;
-    }
-
-    files.forEach((file) => {
-        if (file.endsWith(".hyl")) {
-            const inputFilePath = path.join(inputFolder, file);
-            const outputFileName = file.replace(/\.hyl$/, "-light.svg");
-            const outputFilePath = path.join(outputFolder, outputFileName);
-
-            const command = `npx @hylimo/cli -f ${inputFilePath} -o ${outputFilePath}`;
-            execSync(command, { stdio: "inherit" });
-
-            const darkOutputFileName = file.replace(/\.hyl$/, "-dark.svg");
-            const darkOutputFilePath = path.join(outputFolder, darkOutputFileName);
-
-            const darkCommand = `npx @hylimo/cli -f ${inputFilePath} -o ${darkOutputFilePath} --dark`;
-            execSync(darkCommand, { stdio: "inherit" });
-
-            console.log(`Rendered ${file} to ${outputFileName} and ${darkOutputFileName}`);
-        }
+    const command = cliBuilder(inputFilePath, outputFilePath);
+    exec(command, {}, () => {
+        console.log(`Rendered ${file} to ${outputFileName}`);
     });
-});
+}
+
+function renderHylimoDiagram(inputFolder, file, outputFolder, variant = "", dark = false) {
+    renderDiagram(
+        inputFolder,
+        file,
+        outputFolder,
+        (inputFilePath, outputFilePath) => `npx @hylimo/cli -f ${inputFilePath} -o ${outputFilePath} ${dark? "--dark" : ""}`,
+        variant
+    );
+}
+
+function renderMermaidDiagram(inputFolder, file, outputFolder, variant = "", dark = false) {
+    renderDiagram(
+        inputFolder,
+        file,
+        outputFolder,
+        (inputFilePath, outputFilePath) =>
+            `npx -p @mermaid-js/mermaid-cli mmdc -i ${inputFilePath} -o ${outputFilePath} ${dark ? "-t dark -b #1b1b1d" : ""}`,
+        variant
+    );
+}
+
+function convertFiles(inputFolder, outputFolder) {
+    fs.readdir(inputFolder, (err, files) => {
+        if (err) {
+            console.error("Error reading directory:", err);
+            return;
+        }
+
+        files.forEach((file) => {
+            if (file.endsWith(".hyl")) {
+                renderHylimoDiagram(inputFolder, file, outputFolder, "light");
+                renderHylimoDiagram(inputFolder, file, outputFolder, "dark", true);
+            } else if (file.endsWith(".mmd")) {
+                renderMermaidDiagram(inputFolder, file, outputFolder, "light");
+                renderMermaidDiagram(inputFolder, file, outputFolder, "dark", true);
+            }
+        });
+    });
+}
+
+//convertFiles("diagrams", "static/renderedDiagrams");
+convertFiles("image-sources/diagrams/user-journeys", "static/renderedDiagrams");
